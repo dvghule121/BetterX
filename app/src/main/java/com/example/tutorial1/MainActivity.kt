77ab0c1aa33179f9a3b2755b9afe3fb54f5df6d8
Dyno <dvghule121@gmail.com>
2022-04-27 23:01:24 +0530

    first commit

--- app/src/main/java/com/example/tutorial1/MainActivity.kt
+++ app/src/main/java/com/example/tutorial1/MainActivity.kt
@@ -5,17 +5,43 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.cardview.widget.CardView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val height = findViewById<TextView>(R.id.height_input)
-        val btn = findViewById<Button>(R.id.submit)
+        val height  =  findViewById<EditText>(R.id.height)
+        val weight = findViewById<EditText>(R.id. weight)
+        val result = findViewById<TextView>(R.id.bmi)
+        val resultText = findViewById<TextView>(R.id.result)
+        val btn = findViewById<Button>(R.id.btn)
 
         btn.setOnClickListener {
-            height.text = "How are you"
+            val bmi = (weight.text.toString().toInt()*10000)/(height.text.toString().toInt()*height.text.toString().toInt())
+            result.text = bmi.toString()
+
+            if (bmi < 18){
+                resultText.text = "You're underweight"
+                findViewById<CardView>(R.id.cardView).setBackgroundResource(R.color.healthy)
+
+            }
+            else if (bmi in 18..25){
+                resultText.text = "You're Healthy"
+                findViewById<CardView>(R.id.cardView).setBackgroundResource(R.color.underweight)
+
+            }
+            else if (bmi in 25..30){
+                resultText.text = "You're overweight"
+                findViewById<CardView>(R.id.cardView).setBackgroundResource(R.color.overweight)
+
+            }
+            else if (bmi > 30){
+                resultText.text = "You're very overweight"
+                findViewById<CardView>(R.id.cardView).setBackgroundResource(R.color.veryweight)
+            }
         }
     }
 
